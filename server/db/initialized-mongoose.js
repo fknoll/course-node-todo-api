ead5aaaf16aae958d23d69334f22e9a795476af3
Freddy Knoll <fk@softwarekompagniet.dk>
2018-10-24 16:10:43 +0200

    Setup app for Heroku

--- server/db/initialized-mongoose.js
+++ server/db/initialized-mongoose.js
@@ -4,7 +4,7 @@ myMongoose.Promise = global.Promise;
 
 // Replacing localhost wit 127.0.0.1
 // reduce time usage and might fixe problems with timeout...
-myMongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true});
+myMongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {
   initializedMongoose: myMongoose
