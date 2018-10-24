3c35dd0d4caa8c2e20b628dbe09deb5cc33ad0aa
Freddy Knoll <fk@softwarekompagniet.dk>
2018-10-24 13:36:35 +0200

    Setup start script and heroku port

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.get('/todos', (req, res) => {
 
 });
 
-// GET /todos/123456
+// GET /todos/:id
 app.get('/todos/:id', (req, res) => {
 
   var id = req.params.id;
@@ -85,7 +85,7 @@ app.get('/todos/:id', (req, res) => {
       };
       res.send({todo});
     })
-    .catch((e) => res.status(400).send(e))
+    .catch((e) => res.status(400).send())
   ;
 
 });
